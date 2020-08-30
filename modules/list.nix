dd2bacc85fe0f361808ce1ceeba9787bc64bf830
Taylor Gunnoe <7254833+tgunnoe@users.noreply.github.com>
2020-08-29 23:37:29 -0400

    Initialize qt515

---
+++ modules/list.nix
@@ -0,0 +1,7 @@
+[
+  ./services/torrent/qbittorrent.nix
+  ./security/mitigations.nix
+  ./services/hardware/wii-u-gc-adapter.nix
+  ./services/games/gamemode.nix
+  ./services/x11/window-managers/steam.nix
+]
